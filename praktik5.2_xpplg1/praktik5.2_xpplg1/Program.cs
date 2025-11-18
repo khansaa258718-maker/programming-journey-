ab3daffeebb023564bd05774b29e5fdceb6ef5ae
khansaa258718-maker <khansaa258718@student.smkn1kandeman.sch.id>
2025-11-18 08:11:08 +0700

    Add files via upload

---
+++ praktik5.2_xpplg1/praktik5.2_xpplg1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik5._2_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable untuk menyimpan hasil penjumlahan
+            int total = 0;
+
+            Console.WriteLine("menghitung jumlah angka dari 1 sampai 5:");
+
+            // perulangan for dari 1 hingga 5
+            for (int i = 1; i <= 5; i++)
+            {
+                // tambahkan nilai i saat ini ke variable total
+                total = total + i;// bisa juga di tulis sebagai:total += i;
+
+                //tampilkan proses penjumlahan di setiap langkah
+                Console.WriteLine("menambahkan" + i + ", total sementara + " + total);
+
+                // tampilkan hasil akhir setelah perulangan selesai
+                Console.WriteLine("\nhasil akhir penjumlahan adalah: " + total);
+
+            }
+        }
+    }
+}
---
+++ praktik_5.7_xpplg1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._7_xpplg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //inisialisasi variable untuk menyimpan input
+            string inputuser = "";
+
+            Console.WriteLine("program akan terus berjalan hingga anda mengetik 'keluar'.");
+
+            // perulangan selama isi variable inputUser TIDAK SAMA DENGAN 'keluar"
+            while (inputuser.ToLower() != "keluar")
+            {
+                Console.Write("\nketik sesuatu (atau  'keluar' untuk berhenti): ");
+                inputuser = Console.ReadLine(); // baca input dari user
+
+                Console.WriteLine("anda mengetik: " + inputuser);
+            }
+            Console.WriteLine("\nprogram selesai. terima kasih!");
+                
+
+            }
+        }
+}
