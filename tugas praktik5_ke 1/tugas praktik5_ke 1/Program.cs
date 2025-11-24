0d99fe647f0e0d60ad809f85c2a3f7a4b882d18d
khansaa258718-maker <khansaa258718@student.smkn1kandeman.sch.id>
2025-11-24 07:45:23 +0700

    Add files via upload

---
+++ tugas praktik5_ke 1/tugas praktik5_ke 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_praktik5_ke_1
+{
+    internal class Progra
+    { }
+
+public class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Tabel Perkalian 5:");
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"5 x {i} = {5 * i}");
+                }
+            }
+        }
+    }
+    
+
---
+++ tugas praktik5_ke2/tugas praktik5_ke2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_praktik5_ke2
+{
+    internal class Progra
+    { }
+
+public class Program
+        {
+            public static void Main(string[] args)
+            {
+                int total = 0;
+                int i = 1;
+
+                while (i <= 5)
+                {
+                    total += i;
+                    i++;
+                }
+
+                Console.WriteLine($"Total = {total}");
+            }
+        }
+    }
+    
