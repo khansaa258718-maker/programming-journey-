945df822334a8f03f253dc8b6a867b0ab2118e69
khansaa258718-maker <khansaa258718@student.smkn1kandeman.sch.id>
2025-11-24 07:57:13 +0700

    Add files via upload

---
+++ praktik5.1/praktik5.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik5._1
+{
+    internal class Program
+    { }
+
+namespace contoh_perulangan_for
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Perulangan untuk menampilkan angka 1 sampai 10:");
+
+                // Perulangan for dimulai di sini
+                for (int i = 1; i <= 10; i++)
+                {
+                    // Blok kode yang akan diulang
+                    Console.WriteLine("Angka ke-" + i);
+                }
+
+                Console.WriteLine("\nPerulangan selesai.");
+            }
+        }
+    }
+}
+  
